89b9fdaa553fe289f2ebcdb7a101f7160a033ffb
rumaan <rumaankalander@gmail.com>
2023-05-10 12:19:39 +0530

    add vercel dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import criticalCSS from "./lib";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [criticalCSS()],
+  adapter: vercel(),
 });
