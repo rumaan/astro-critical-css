483e4b168da0fb6d044f44e71de1c1134171e9e8
Dan Levy <justsml@gmail.com>
2024-09-06 16:45:55 -0600

    feat(options): add critical options passthrough for plugin config

--- lib/index.ts
+++ lib/index.ts
@@ -1,27 +1,59 @@
 import type { AstroIntegration } from "astro";
 import { fileURLToPath } from "node:url";
+import { statSync } from "node:fs";
 import { join } from "node:path";
-import { generate } from "critical";
+import { generate, type GenerateFuncArgs } from "critical";
 import fg from "fast-glob";
 import { writeFile } from "node:fs/promises";
+import debug from "debug";
 
-export default (): AstroIntegration => {
+const log = debug("astro-plugin-critical-css");
+
+type PluginOptions = Omit<GenerateFuncArgs, "src" | "base" | "inline"> & {
+  /** silence console output */
+  quiet?: boolean;
+};
+
+export default (
+  options: PluginOptions | undefined = {}
+): AstroIntegration => {
   return {
     name: "critical-css",
     hooks: {
       "astro:build:done": async ({ dir }) => {
+        let totalInputSize = 0;
+        let totalOutputSize = 0;
+        let fileCount = 0;
         const distPath = fileURLToPath(dir);
         const htmlPathsStream = fg.stream("**/*.html", { cwd: distPath });
+        const startTime = Date.now();
+        log("🪄 Starting: Inlining CSS in path %s", distPath);
         for await (const htmlPath of htmlPathsStream) {
           const path = join(distPath, htmlPath.toString());
-          console.log("🧷 Inlining =>", path);
+          if (!options.quiet) console.log("🧷 Inlining =>", path);
+          const initialSize = statSync(path).size;
+          totalInputSize += initialSize;
+          fileCount++;
           // Execute critical generate
           const { html } = await generate({
             inline: true,
             src: path,
             base: distPath,
+            ...options,
           });
-          await writeFile(path, Buffer.from(html, "utf-8"));
+          const htmlData = Buffer.from(html, "utf-8");
+          totalOutputSize += htmlData.length;
+          log("Html difference in bytes +/-: %d", htmlData.length - initialSize);
+          await writeFile(path, htmlData);
+        }
+        const endTime = Date.now();
+        log("Total input size: %s bytes", totalInputSize.toLocaleString());
+        log("Total output size: %s bytes", totalOutputSize.toLocaleString());
+        log("Total difference in bytes +/-: %s", (totalOutputSize - totalInputSize).toLocaleString());
+        log("✅ Done: Inlining CSS in %d sec.", endTime - startTime);
+        if (!options.quiet) {
+          console.log("Total bytes input & output (difference): %skB & %skB, (+/- %skB)", (totalInputSize/1024).toLocaleString(), (totalOutputSize/1024).toLocaleString(), ((totalOutputSize - totalInputSize)/1024).toLocaleString());
+          console.log("Done: Inlined CSS in %d file(s) in %d sec.", fileCount, (endTime - startTime) / 1000);
         }
       },
     },
--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,8 +1,96 @@
 declare module "critical" {
+
+  /* Generate function arguments - src: https://github.com/addyosmani/critical/tree/master?tab=readme-ov-file#options */
   export type GenerateFuncArgs = {
-    inline: boolean;
-    base: string;
-    src: string;
+    /* Inline critical-path CSS or configure inline-critical options */
+    inline?: boolean | InlineCriticalOptions;
+    /* Base directory for source and destination */
+    base?: string;
+    /* HTML source to operate against */
+    html?: string;
+    /* Array of CSS file paths, file globs, Vinyl objects, or source CSS strings */
+    css?: (string | unknown)[];
+    /* Location of the HTML source. */
+    src?: string;
+    /* Location of where to save the output of an operation. Use an object with 'html' and 'css' props if you want to store both. */
+    target?: string | { html?: string; css?: string, uncritical?: string };
+    /* Viewport width */
+    width?: number;
+    /* Viewport height */
+    height?: number;
+    /* Array of dimensions. Takes precedence over `width` and `height` if set. */
+    dimensions?: { width: number; height: number }[];
+    /* Remove the inlined styles from any stylesheets referenced in the HTML.
+     * It generates new references based on extracted content so it's safe to
+     * use for multiple HTML files referencing the same stylesheet. Use with
+     * caution. Removing the critical CSS per page results in a unique async
+     * loaded CSS file for every page. Meaning you can't rely on cache across
+     * multiple pages. */
+    extract?: boolean;
+    /* Inline images */
+    inlineImages?: boolean;
+    /* List of directories/urls for assets lookup */
+    assetPaths?: string[];
+    /* Max file size for base64 inlined images */
+    maxImageFileSize?: number;
+    /* Custom rebase options or function */
+    rebase?: RebaseOptions | ((url: string) => string);
+    /* Ignore CSS rules (array or object) */
+    ignore?: (string | IgnoreOptions)[] | IgnoreOptions;
+    /* Ignore inlined stylesheets */
+    ignoreInlinedStyles?: boolean;
+    /* User agent to use when fetching remote src */
+    userAgent?: string;
+    /* Penthouse configuration options */
+    penthouse?: object;
+    /* Configuration options for got */
+    request?: object;
+    /* Configuration options for CleanCSS */
+    cleanCSS?: object;
+    /* Basic authorization: user */
+    user?: string;
+    /* Basic authorization: pass */
+    pass?: string;
+    /* Throw error on CSS parsing errors */
+    strict?: boolean;
+  };
+
+  type InlineCriticalOptions = {
+    /**
+     * PreloadStrategy:
+     * - **default:** Move stylesheet links to the end of the document and insert preload meta tags in their place.
+     * - **"body":** Move all external stylesheet links to the end of the document.
+     * - **"media":** Load stylesheets asynchronously by adding `media="print"` and swap to `media="all"` once loaded (https://www.filamentgroup.com/lab/load-css-simpler/). _[JS]_
+     * - **"swap":** Convert stylesheet links to preloads that swap to `rel="stylesheet"` once loaded. _[JS]_
+     * - **"polyfill":** Inject [LoadCSS](https://github.com/filamentgroup/loadCSS) and use it to load stylesheets. _[JS]_
+     */
+    strategy?: 'default' | 'body' | 'media' | 'swap' | 'polyfill';
+    /** will remove the inlined styles from any stylesheets referenced in the HTML */
+    extract?: boolean;
+    /** base path for extracting styles */
+    basePath?: string;
+    /** ignore matching stylesheets when inlining */
+    ignore?: string[];
+    /** defines the element used by loadCSS as a reference for inlining. */
+    selector?: string;
+    /** specifies position of noscript fallback ('body' - end of body, 'head' - end of head, false - no noscript) */
+    noscript?: boolean | 'body' | 'head';
+    /** takes an array of stylesheet hrefs to replace the original links in the document */
+    replaceStylesheets?: string[];
+  };
+  
+  type RebaseOptions = {
+    from: string;
+    to: string;
+  } | ((url: string | unknown) => string);
+  
+  type IgnoreOptions = {
+    /* array of ignored @rules */
+    atrule?: string[];
+    /* array of ignored rules (strings or regex) */
+    rule?: (string | RegExp)[];
+    /* function to ignore specific declarations */
+    decl?: (node: any, value: string) => boolean;
   };
 
   export type Result = {
