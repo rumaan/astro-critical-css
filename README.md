52cac5a13e7ea5c3b46f8d9619624891b06aa700
Dan Levy <justsml@gmail.com>
2024-09-14 17:32:41 -0600

    chore: add support for passing options to criticalCSS

--- README.md
+++ README.md
@@ -33,7 +33,90 @@ export default defineConfig({
 });
 ```
 
-During your build this integration will look at all the static HTML files and run it through [Critical](https://github.com/addyosmani/critical).
+During build this integration will look at all the static HTML files and run them through [Critical](https://github.com/addyosmani/critical). (Which depends on Puppeteer/Chromium.)
+
+## Options
+
+If you need options, `criticalCSS` supports the following config options:
+
+```ts
+// Options
+criticalCSS({
+  /** Silence log output (plugin specific, everything else is for criticalCSS) */
+  silent: boolean,
+  /* HTML source to operate against */
+  html: string,
+  /* Array of CSS file paths, file globs, Vinyl objects, or source CSS strings */
+  css: (string | unknown)[],
+  /* Location of where to save the output of an operation. Use an object with 'html' and 'css' props if you want to store both. */
+  target: string | { html: string, css: string, uncritical: string },
+  /* Viewport width */
+  width: number,
+  /* Viewport height */
+  height: number,
+  /* Array of dimensions. Takes precedence over `width` and `height` if set. */
+  dimensions: { width: number, height: number }[],
+  /* Remove the inlined styles from any stylesheets referenced in the HTML.
+    * It generates new references based on extracted content so it's safe to
+    * use for multiple HTML files referencing the same stylesheet. Use with
+    * caution. Removing the critical CSS per page results in a unique async
+    * loaded CSS file for every page. Meaning you can't rely on cache across
+    * multiple pages. */
+  extract: boolean,
+  /* Inline images */
+  inlineImages: boolean,
+  /* List of directories/urls for assets lookup */
+  assetPaths: string[],
+  /* Max file size for base64 inlined images */
+  maxImageFileSize: number,
+  /* Custom rebase options or function */
+  rebase: RebaseOptions | ((url: string) => string),
+  /* Ignore CSS rules (array or object) */
+  ignore: (string | IgnoreOptions)[] | IgnoreOptions,
+  /* Ignore inlined stylesheets */
+  ignoreInlinedStyles: boolean,
+  /* User agent to use when fetching remote src */
+  userAgent: string,
+  /* Penthouse configuration options */
+  penthouse: object,
+  /* Configuration options for got */
+  request: object,
+  /* Configuration options for CleanCSS */
+  cleanCSS: object,
+  /* Basic authorization: user */
+  user: string,
+  /* Basic authorization: pass */
+  pass: string,
+  /* Throw error on CSS parsing errors */
+  strict: boolean,
+});
+```
+
+## Logging
+
+By default, `astro-critical-css` will log the output of the operation.
+
+![default console output](./public/default-logging.jpg)
+
+If you want to silence the logs, you can pass the `silent` option:
+
+```ts
+import criticalCSS from "astro-critical-css";
+
+export default defineConfig({
+  integrations: [criticalCSS({ silent: true })],
+});
+```
+
+If you want verbose logging, set the `DEBUG` environment variable:
+
+```sh
+DEBUG=astro-critical-css npx astro build
+# or
+DEBUG=astro* npx astro build
+```
+
+![debug console output](./public/debug-logging.jpg)
 
 ## Astro SSR Mode
 
--- lib/index.ts
+++ lib/index.ts
@@ -3,26 +3,31 @@ import { fileURLToPath } from "node:url";
 import { statSync } from "node:fs";
 import { join } from "node:path";
 import { generate, type GenerateFuncArgs } from "critical";
-import fg from "fast-glob";
+import * as fg from "fast-glob";
 import { writeFile } from "node:fs/promises";
 import debug from "debug";
 
-const log = debug("astro-plugin-critical-css");
+const log = debug("astro-critical-css");
 
-type PluginOptions = Omit<GenerateFuncArgs, "src" | "base" | "inline"> & {
+export type PluginOptions = Omit<
+  GenerateFuncArgs,
+  "src" | "base" | "inline"
+> & {
   /** silence console output */
-  quiet?: boolean;
+  silent?: boolean;
 };
 
-export default (
-  options: PluginOptions | undefined = {}
-): AstroIntegration => {
+export default ({
+  silent,
+  ...options
+}: Partial<PluginOptions> | undefined = {}): AstroIntegration => {
+  log("Options: %o", options);
   return {
     name: "critical-css",
     hooks: {
       "astro:build:done": async ({ dir }) => {
-        let totalInputSize = 0;
-        let totalOutputSize = 0;
+        let htmlInputSize = 0;
+        let htmlOutputSize = 0;
         let fileCount = 0;
         const distPath = fileURLToPath(dir);
         const htmlPathsStream = fg.stream("**/*.html", { cwd: distPath });
@@ -30,32 +35,63 @@ export default (
         log("🪄 Starting: Inlining CSS in path %s", distPath);
         for await (const htmlPath of htmlPathsStream) {
           const path = join(distPath, htmlPath.toString());
-          if (!options.quiet) console.log("🧷 Inlining =>", path);
+          if (!silent) console.log("🧷 Inlining =>", path);
           const initialSize = statSync(path).size;
-          totalInputSize += initialSize;
+          htmlInputSize += initialSize;
           fileCount++;
-          // Execute critical generate
-          const { html } = await generate({
+          // Execute criticalCss' generate
+          const results = await generate({
             inline: true,
             src: path,
             base: distPath,
             ...options,
-          });
+          }).catch((err) => ({ error: err }));
+
+          let html = "html" in results ? results.html : "";
+
+          if ("error" in results) {
+            console.error("Error inlining CSS:", results?.error);
+            log("Error inlining CSS: %o", results?.error);
+            throw results.error;
+          }
+
           const htmlData = Buffer.from(html, "utf-8");
-          totalOutputSize += htmlData.length;
-          log("Html difference in bytes +/-: %d", htmlData.length - initialSize);
+          htmlOutputSize += htmlData.length;
+          log(
+            "Html difference in bytes +/-: %d",
+            htmlData.length - initialSize
+          );
           await writeFile(path, htmlData);
         }
         const endTime = Date.now();
-        log("Total input size: %s bytes", totalInputSize.toLocaleString());
-        log("Total output size: %s bytes", totalOutputSize.toLocaleString());
-        log("Total difference in bytes +/-: %s", (totalOutputSize - totalInputSize).toLocaleString());
-        log("✅ Done: Inlining CSS in %d sec.", endTime - startTime);
-        if (!options.quiet) {
-          console.log("Total bytes input & output (difference): %skB & %skB, (+/- %skB)", (totalInputSize/1024).toLocaleString(), (totalOutputSize/1024).toLocaleString(), ((totalOutputSize - totalInputSize)/1024).toLocaleString());
-          console.log("Done: Inlined CSS in %d file(s) in %d sec.", fileCount, (endTime - startTime) / 1000);
+        logSummary(htmlInputSize, htmlOutputSize, startTime);
+        if (!silent) {
+          console.log(
+            "HTML bytes (in & out) %skB & %skB, (+/- %skB)",
+            (htmlInputSize / 1024).toLocaleString(),
+            (htmlOutputSize / 1024).toLocaleString(),
+            ((htmlOutputSize - htmlInputSize) / 1024).toLocaleString()
+          );
+          console.log(
+            "🪄 Done: Inlined CSS in %d file(s) in %d sec using options: %o",
+            fileCount,
+            (endTime - startTime) / 1000,
+            options,
+          );
         }
       },
     },
   };
 };
+function logSummary(htmlInputSize: number, htmlOutputSize: number, startTime: number) {
+  const endTime = Date.now();
+  log("HTML input size: %s bytes", htmlInputSize.toLocaleString());
+  log("HTML output size: %s bytes", htmlOutputSize.toLocaleString());
+  log(
+    "HTML difference in bytes +/-: %s",
+    (htmlOutputSize - htmlInputSize).toLocaleString()
+  );
+  log("✅ Done: Inlining CSS in %d sec.", endTime - startTime);
+  return endTime;
+}
+
